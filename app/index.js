8aca8f49b3f2ff9536c30f26dbaa6b624f0fa996
jacqlynb <jacqlyn.bender@gmail.com>
2019-01-26 21:27:30 -0500

    add infowindow feature for map pins

--- app/index.js
+++ app/index.js
@@ -1,15 +1,5 @@
 // javascript lives here
 
-// Works
-const fetchedData = fetch('test.json').then(function(response) { 
-    return response.json();
-    }).then(data => console.log(data[0])); 
-
-// THIS WORKS!!!!! WHY DOESN'T IT WORK FOR SETTING MAP POSITION?
-
-
-
-
 // initialize and add the map
 function initMap() {
 
@@ -18,13 +8,13 @@ function initMap() {
 
   // the map, centered at Montreal
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 11, center: montreal});
+      document.getElementById('map'), {zoom: 12, center: montreal});
 
   // create a marker
   var marker = new google.maps.Marker({position: montreal, map: map});
 
   // iterate through JSON array
-  fetch('test.json').then(function(response) {
+  fetch('archive.json').then(function(response) {
     return response.json();
     }).then(function(data) { 
         for(var i = 0; i < data.length; i++) {
@@ -35,27 +25,16 @@ function initMap() {
               content: contentString
             });
 
-          var marker = new google.maps.Marker({position: {lat: parseFloat(data[i]["Latitude"]), lng: parseFloat(data[i]["Longitude"])}, map: map});
+          var marker = new google.maps.Marker({
+              position: {lat: parseFloat(data[i]["Latitude"]), lng: parseFloat(data[i]["Longitude"])}, 
+              map: map,
+              info: contentString
+            });
           
-          marker.addListener('click', function() {
-            infowindow.open(map, marker);
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(this.info);
+            infowindow.open(map, this);
           });
         }
     });
-
-//   fetch('test.json').then(function(response) {
-//         return response.json();
-//         }).then(function(data) { console.log(parseFloat(data[10]["Longitude"])) })
-
-  
-
-    // create a marker
-    // var marker = new google.maps.Marker({position: {lat: fetch('test.json').then(function(response) {
-    //     return response.json();
-    //     }).then(function(data) { return (parseFloat(data[i]["Latitude"])) }), 
-    //     lng: fetch('test.json').then(function(response) {
-    //       return response.json();
-    //       }).then(function(data) { return (parseFloat(data[i]["Longitude"])) })
-    //   }, map: map});
-
 }
